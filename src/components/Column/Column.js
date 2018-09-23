05088e8c92cdac6253b4028b2e39f0e9eb33afad
iamoperand <narora200@gmail.com>
2018-09-23 14:02:02 +0530

    [Feat]: Task component created

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,19 +1,47 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Task from '../Task';
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-redius: 2px;
+`;
+
+const Title = styled.div`
+  padding: 8px;
+`;
+
+const TaskList = styled.div`
+  padding: 8px;
+`;
 
 class Column extends Component {
   render() {
     const {
-      column: { title }
+      column: { title },
+      tasks
     } = this.props;
-    return title;
+    return (
+      <Container>
+        <Title>{title}</Title>
+        <TaskList>
+          {tasks.map(task => (
+            <Task key={task.id} task={task} />
+          ))}
+        </TaskList>
+      </Container>
+    );
   }
 }
 
 Column.propTypes = {
   column: PropTypes.shape({
     title: PropTypes.string
-  }).isRequired
+  }).isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.shape()).isRequired
 };
 
 export default Column;
