c127d70571bf9e968db983603cf4dd172cbe2756
iamoperand <narora200@gmail.com>
2018-09-23 13:18:46 +0530

    [Feat]: Column component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
+import Column from './components/Column';
+import data from './data';
+
 class App extends Component {
+  state = data;
+
   render() {
-    return <div>App</div>;
+    const { tasks, columns, columnOrder } = this.state;
+    return columnOrder.map(columnID => {
+      const column = columns[columnID];
+
+      const columnTasks = column.taskIds.map(taskID => tasks[taskID]);
+
+      return <Column key={column.id} column={column} tasks={columnTasks} />;
+    });
   }
 }
 
